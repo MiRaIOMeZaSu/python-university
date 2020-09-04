d7e53400427324bce855060fe2ae9c542870b818
miraiomezasu <379297266@qq.com>
2020-09-04 23:25:25 +0800

    语法分析LL1优化

--- code/语法分析LL1.py
+++ code/语法分析LL1.py
@@ -15,18 +15,18 @@
     i,+,*,(,),#
 """
 
-
+import re
 from prettytable import PrettyTable
-table = PrettyTable(["步骤", "分析栈", "当前输入a","剩余输入串", "所用产生式"])
+table = PrettyTable(["步骤", "分析栈", "当前入栈", "剩余输入串", "所用产生式"])
 
 # 构造预测分析表
 dists = {
-    ('E', 'i'): 'TH',('E', '('): 'TH',('H', '+'): '+TH',
-    ('H', ')'): 'e',('H', '#'): 'e',('T', 'i'): 'FY',
-    ('T', '('): 'FY',('Y', '+'): 'e',('Y', '*'): '*FY',
-    ('Y', ')'): 'e',('Y', '#'): 'e',('F', 'i'): 'i',
+    ('E', 'i'): 'TH', ('E', '('): 'TH', ('H', '+'): '+TH',
+    ('H', ')'): 'e', ('H', '#'): 'e', ('T', 'i'): 'FY',
+    ('T', '('): 'FY', ('Y', '+'): 'e', ('Y', '*'): '*FY',
+    ('Y', ')'): 'e', ('Y', '#'): 'e', ('F', 'i'): 'i',
     ('F', '('): '(E)',
-    }
+}
 
 # 构造终结符集合
 Vt = ('i', '+', '*', '(', ')')
@@ -35,86 +35,56 @@ Vt = ('i', '+', '*', '(', ')')
 Vh = ('E', 'H', 'T', 'Y', 'F')
 
 
-# 获取输入栈中的内容
-def printstack(stack):
-    rtu = ''
-    for i in stack:
-        rtu += i
-    return rtu
-
-
-# 得到输入串剩余串
-def printstr(str, index):
-    rtu = ''
-    for i in range(index, len(str), 1):
-        rtu += str[i]
-    return rtu
-
-
-# 定义error函数
-def error():
-    print('Error')
-    exit()
-
-
 # 总控程序
-def masterctrl(str):
-    '''
-    总控程序，用于进程文法的判断
-    '''
+def main(sentence):
+    '''总控程序，用于进程文法的判断'''
     # 用列表模拟栈
     stack = []
     location = 0
     # 将#号入栈
-    stack.append(str[location])
+    stack.append(sentence[location])
 
     # 将文法开始符入栈
     stack.append('E')
     # 将输入串第一个字符读进a中
     location += 1
-    a = str[location]
+    input_bottom = sentence[location]
 
-    flag = True
-    count = 1       #计算步骤
-    table.add_row([count, printstack(stack),a, printstr(str, location),''])
-    while flag:
-        if count == 1:
-            pass
-        else:
-            if x in Vt:
-                table.add_row([count, printstack(stack),a, printstr(str, location),''])
-            else:
-                temp = x + '->' + s
-                table.add_row([count, printstack(stack),a, printstr(str, location),temp])
-        x = stack.pop()
-        if x in Vt:          #栈顶是终结符
-            if x == str[location]:   #该字符匹配，输入串向后挪一位
-                location += 1
-                a = str[location]
-            else:            #否则错误
-                error()
-        elif x == '#':       #栈顶是结束符
-            if x == a:       #当前输入字符也是结束符，分析结束
-                flag = False
-            else:            #否则错误
-                error()
-        elif (x, a) in dists.keys():    #M[x,a]是产生式
-            s = dists[(x, a)]
-            for i in range(len(s) - 1, -1, -1):         #倒序入栈
-                if s[i] != 'e':
-                    stack.append(s[i])
-        else:
-            error()
+    count = 1  # 计算步骤
+    table.add_row([count, ''.join(stack),
+                   input_bottom, sentence[location:], ''])
+    stack_top = ''
+    cell = ''
+    while True:
         count += 1
+        stack_top = stack.pop()
+        if stack_top in Vt:  # 栈顶是终结符
+            if stack_top == sentence[location]:  # 该字符匹配，输入串向后挪一位
+                location += 1
+                input_bottom = sentence[location]
+            else:  # 否则错误
+                raise Exception("未知字符错误")
+            table.add_row([count, ''.join(stack), input_bottom,
+                           sentence[location:], ''])
+        else:
+            if stack_top == '#' and stack_top == input_bottom:  # 栈顶是结束符且当前输入字符也是结束符，分析结束
+                break
+            elif (stack_top, input_bottom) in dists.keys():  # M[x,a]是产生式
+                cell = dists[(stack_top, input_bottom)]
+                for i in range(len(cell) - 1, -1, -1):  # 倒序入栈
+                    if cell[i] != 'e':
+                        stack.append(cell[i])
+            else:
+                raise Exception("未知错误")
+            expr = stack_top + '->' + cell
+            table.add_row([count, ''.join(stack), input_bottom,
+                           sentence[location:], expr])
 
 
 if __name__ == '__main__':
-    str = '#i+i#'
-    masterctrl(str)
-    table.align['步骤'] = 'l'
-    table.align['分析栈'] = 'l'
-    table.align['剩余输入串'] = 'l'
-    table.align['所用产生式'] = 'l'
-    table.align['当前输入a'] = 'l'
+    sentence = '#i+i*i#'
+    main(sentence)
+    # 设置左对齐
+    for key in table.align.keys():
+        table.align[key] = 'l'
     print(table)
-    print("分析成功!")
